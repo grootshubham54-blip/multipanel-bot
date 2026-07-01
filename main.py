7b66349de8d4cce21a1e1fbc7c1f96693f2715e4
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-01 16:39:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,38 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+from database import create_tables, add_user
+
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+
+    add_user(
+        user.id,
+        user.username or "No Username"
+    )
+
     await update.message.reply_text(
         "👑 Welcome to KING iOS Bot\n\n"
-        "Use the menu to continue."
+        "Your account has been created."
     )
 
 
 def main():
+    create_tables()
+
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        CommandHandler("start", start)
+    )
 
     print("Bot is running...")
+
     app.run_polling()
 
 
