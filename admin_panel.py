7ab66120db1ee3fa47f7a1f9a5d5b22b438501b8
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-03 00:29:38 +0530

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -1,10 +1,7 @@
 from telegram import ReplyKeyboardMarkup
 
 def admin_keyboard():
-    # नाम बिल्कुल वही रखें जो main.py में if-elif में हैं
-    keyboard = [["🔑 Add Keys", "📦 Stock"], ["👥 Total Users", "🔙 Back to Admin"]]
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    return ReplyKeyboardMarkup([["🔑 Add Keys", "📦 Stock"], ["👥 Total Users", "🔙 Back to Admin"]], resize_keyboard=True)
 
 def admin_game_selection_keyboard():
-    games = [["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀"], ["🔙 Back to Admin"]]
-    return ReplyKeyboardMarkup(games, resize_keyboard=True)
+    return ReplyKeyboardMarkup([["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["🔙 Back to Admin"]], resize_keyboard=True)
