81607909f7b6e1b07def749703a47dbb8b5e9e13
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-02 23:11:27 +0530

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -2,14 +2,25 @@ from telegram import ReplyKeyboardMarkup
 
 def admin_keyboard():
     return ReplyKeyboardMarkup([
-        ["🔑 Add Keys"],
         ["📦 Stock"],
-        ["👥 Total Users"],
-        ["🔙 Back to Main"]
+        ["🎮 Games"],
+        ["🛠 Admin Panel"]
     ], resize_keyboard=True)
 
+
 def admin_game_selection_keyboard():
-    return ReplyKeyboardMarkup([["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"], ["🔙 Back to Admin"]], resize_keyboard=True)
+    return ReplyKeyboardMarkup([
+        ["👑 KING iOS", "WINIOS"],
+        ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"],
+        ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"],
+        ["🔙 Back"]
+    ], resize_keyboard=True)
+
 
 def admin_plan_selection_keyboard():
-    return ReplyKeyboardMarkup([["1 Day", "1 Week"], ["1 Month"], ["🔙 Back to Admin"]], resize_keyboard=True)
+    return ReplyKeyboardMarkup([
+        ["1 Day"],
+        ["1 Week"],
+        ["1 Month"],
+        ["🔙 Back"]
+    ], resize_keyboard=True)
