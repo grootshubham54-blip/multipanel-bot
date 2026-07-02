82c572d9cefed27873cb5347eab4a52fca4fe5cc
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-02 21:26:06 +0530

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -9,7 +9,6 @@ def admin_keyboard():
     ], resize_keyboard=True)
 
 def admin_game_selection_keyboard():
-    # गेम के नाम वही रखें जो main.py की GAME_PLANS में हैं
     return ReplyKeyboardMarkup([["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"], ["🔙 Back to Admin"]], resize_keyboard=True)
 
 def admin_plan_selection_keyboard():
