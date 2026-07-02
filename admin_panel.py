100daef952b7032de6cb8de039ebd64d0e570d9e
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-03 01:10:51 +0530

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -1,24 +1,10 @@
 from telegram import ReplyKeyboardMarkup
 
 def admin_keyboard():
-    # मुख्य एडमिन बटन्स
-    return ReplyKeyboardMarkup([
-        ["🔑 Add Keys", "📦 Stock"], 
-        ["👥 Total Users", "🔙 Back to Admin"]
-    ], resize_keyboard=True)
+    return ReplyKeyboardMarkup([["🔑 Add Keys", "📦 Stock"], ["👥 Total Users", "🔙 Back to Admin"]], resize_keyboard=True)
 
 def admin_game_selection_keyboard():
-    # गेम्स की लिस्ट
-    return ReplyKeyboardMarkup([
-        ["👑 KING iOS", "WINIOS"], 
-        ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], 
-        ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"], 
-        ["🔙 Back to Admin"]
-    ], resize_keyboard=True)
+    return ReplyKeyboardMarkup([["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"], ["🔙 Back to Admin"]], resize_keyboard=True)
 
 def admin_plan_selection_keyboard():
-    # कीज ऐड करते समय प्लान चुनने के लिए
-    return ReplyKeyboardMarkup([
-        ["1 Day", "1 Week", "1 Month"], 
-        ["🔙 Back to Admin"]
-    ], resize_keyboard=True)
+    return ReplyKeyboardMarkup([["1 Day", "1 Week", "1 Month"], ["🔙 Back to Admin"]], resize_keyboard=True)
