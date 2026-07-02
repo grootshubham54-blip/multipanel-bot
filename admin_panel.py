ac3f8214cd9737ce0785577e2a0cfee7990fcf6a
grootshubham54-blip <grootshubham54@gmail.com>
2026-07-03 00:00:59 +0530

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -1,4 +1,4 @@
-from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram import ReplyKeyboardMarkup
 
 def admin_keyboard():
     keyboard = [
@@ -8,8 +8,13 @@ def admin_keyboard():
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 def admin_game_selection_keyboard():
-    # आपके main.py में मौजूद गेम्स के अनुसार
-    games = [["👑 KING iOS", "WINIOS"], ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀", "DOLPHIN IOS"], ["🔙 Back to Admin"]]
+    # यहाँ वही नाम रखें जो main.py की GAME_PLANS डिक्शनरी में हैं
+    games = [
+        ["👑 KING iOS", "WINIOS"], 
+        ["NEXT IOS", "𝐌𝐚𝐫𝐬 𝐋𝐨𝐚𝐝𝐞𝐫"], 
+        ["𝘿𝙀𝘼𝘿𝙀𝙀𝙀𝙔𝙀"], 
+        ["🔙 Back to Admin"]
+    ]
     return ReplyKeyboardMarkup(games, resize_keyboard=True)
 
 def admin_plan_selection_keyboard():
